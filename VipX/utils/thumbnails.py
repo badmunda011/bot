0cc1069afac8c70aafc73269d1b0dd5817f596eb
PB65-Aujla <140618546+PB65-Aujla@users.noreply.github.com>
2023-09-16 16:54:10 +0400

    Update thumbnails.py

--- VipX/utils/thumbnails.py
+++ VipX/utils/thumbnails.py
@@ -125,7 +125,7 @@ async def gen_thumb(videoid, chat_id):
         try:
             draw.text(
                 (25, 0.1),
-                f"VIP  MUSIC",
+                f"AUJLA  MUSIC",
                 fill="white",
                 stroke_width=4,
                 stroke_fill="black",
@@ -265,7 +265,7 @@ async def gen_qthumb(videoid, chat_id):
         try:
             draw.text(
                 (25, 0.1),
-                "VIP  MUSIC",
+                "AUJLA  MUSIC",
                 fill="white",
                 stroke_width=5,
                 stroke_fill="black",
